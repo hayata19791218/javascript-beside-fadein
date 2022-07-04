52a36f8f512d1caa016751c5a373e28232a6f6c8
uheijonio <uheisugakubu@gmail.com>
2022-07-04 11:35:52 +0900

    データのアップ

---
+++ js/common.js
@@ -0,0 +1,20 @@
+let fadeInTarget = document.querySelectorAll('.fade-in');
+
+  for (let i = 0; i < fadeInTarget.length; i++){
+
+    window.addEventListener('scroll', function(){
+        
+        const rect = fadeInTarget[i].getBoundingClientRect().top;
+        const scroll = window.pageYOffset;
+        const offset = rect + scroll;
+        const windowHeight = window.innerHeight; 
+        
+        if(scroll > offset - windowHeight + 150){
+            fadeInTarget[i].classList.add('scroll-in');
+        }
+        else{
+            fadeInTarget[i].classList.remove('scroll-in');
+        }
+        
+    });
+  }
